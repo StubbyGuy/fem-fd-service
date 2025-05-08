dae48e0b8ff85608c975d6d5add32fe8c26d3b14
Eric <180332937+StubbyGuy@users.noreply.github.com>
2025-05-08 11:24:00 -0500

    updating terraform baseline tf files

--- workshop/terraform/backend.tf
+++ workshop/terraform/backend.tf
@@ -1,1 +1,7 @@
-#placeholder
+terraform {
+  backend "s3" {
+    bucket = "fem-fd-service-eric"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
--- workshop/terraform/locals.tf
+++ workshop/terraform/locals.tf
@@ -1,1 +1,3 @@
-#placeholder
+locals {
+  bastion_ingress = ["<ip-address>/32"]
+}
